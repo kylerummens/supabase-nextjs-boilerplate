d0bfbe33037455323de4b109ce7583bfe3ede5dc
Kyle Rummens <k.d.rummens@gmail.com>
2023-05-08 22:32:24 +0200

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,47 @@
+import 'server-only';
+
+import '../styles/globals.scss';
+import 'react-tooltip/dist/react-tooltip.css';
+
+import { Inter } from 'next/font/google';
+import { createClient } from '@/utils/server/supabase-server';
+import SupabaseProvider from '@/components/supabase-provider';
+import SupabaseListener from '@/components/supabase-listener';
+const inter = Inter({
+    subsets: ['latin'],
+    variable: '--font-inter',
+    display: 'swap',
+});
+
+// do not cache this layout
+export const revalidate = 0;
+
+export default async function RootLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    const supabase = createClient();
+
+    const {
+        data: { session },
+    } = await supabase.auth.getSession();
+
+    return (
+        <html
+            lang="en"
+            style={{ colorScheme: 'light dark' }}
+            className={inter.variable}
+        >
+            <head></head>
+            <body>
+                <SupabaseProvider>
+                    <SupabaseListener
+                        serverAccessToken={session?.access_token}
+                    />
+                    {children}
+                </SupabaseProvider>
+            </body>
+        </html>
+    );
+}
